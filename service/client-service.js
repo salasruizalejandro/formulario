3b6fabe8abb86db88c8ab750690008f9b88bf8cc
alejandro <salasruizalejandro1227@gmail.com>
2022-09-19 16:44:07 -0500

    cargar datos con Open y Onload

--- service/client-service.js
+++ service/client-service.js
@@ -4,16 +4,22 @@ const crearNuevaLinea = (nombre, correoElectronico, edad, productodeInteres, rec
     const linea = document.createElement("div");
     const content = `
             <h3>NumeroCliente</h3>
-            <h3>Nombre</h3>
-            <h3>correo electronico</h3>
-            <h3>edad</h3>
-            <h3>Producto Interes</h3>
-            <h3>recomendaciones</h3>
-            <h3>medio de comunicacion</h3>
-            <h3>que producto necesita</h3>
-            <h3>comentarios</h3>
+            <h3>Nombre:  ${nombre}</h3>
+            <h3>correo electronico:  ${correoElectronico}</h3>
+            <h3>edad: ${edad}</h3>
+            <h3>Producto Interes: ${productodeInteres}</h3>
+            <h3>recomendaciones: ${recomendacion}</h3>
+            <h3>medio de comunicacion: ${mediodeComunicacion}</h3>
+            <h3>que producto necesita: ${queproductoNecesita}</h3>
+            <h3>comentarios: ${comentarios}</h3>
     `;
+    linea.innerHTML = content;
+    return linea;
 }
+
+// 6.recorrer todo el arbol de Dom y llevarnos al elemento data-tabla
+
+const tabla = document.querySelector("[data-tabla]")
 
 
 
@@ -35,6 +41,10 @@ http.send()
 
 //4 recargar datos de json en consola de Cliente-Servicio
 http.onload = () =>{
-    const data = http.response
-    console.log(data)
+    const data = JSON.parse(http.response)
+    //7. se recibe los perfiles de cada uno de los clientes en j.son
+    data.forEach( perfil => {
+        const nuevaLinea = crearNuevaLinea(perfil.nombre, perfil.correoElectronico, perfil.edad, perfil.productodeInteres, perfil.recomendacion, perfil.mediodeComunicacion, perfil.queproductoNecesita, perfil.comentarios)
+        tabla.appendChild(nuevaLinea)
+    });
 }
