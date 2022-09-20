7dbd1f5da0a350413ba2da5562d7d0b63ba4592f
alejandro <salasruizalejandro1227@gmail.com>
2022-09-20 15:20:02 -0500

    creacion de Controllers y cliente-service

--- service/client-service.js
+++ service/client-service.js
@@ -1,32 +1,4 @@
 
-//5. funcion crear nueva linea donde se incluye los datos al html
-const crearNuevaLinea = (nombre, correoElectronico, edad, productodeInteres, recomendacion, mediodeComunicacion, queproductoNecesita, comentarios) =>{
-    const linea = document.createElement("div");
-    const content = `
-            <h3>NumeroCliente</h3>
-            <h3>Nombre:  ${nombre}</h3>
-            <h3>correo electronico:  ${correoElectronico}</h3>
-            <h3>edad: ${edad}</h3>
-            <h3>Producto Interes: ${productodeInteres}</h3>
-            <h3>recomendaciones: ${recomendacion}</h3>
-            <h3>medio de comunicacion: ${mediodeComunicacion}</h3>
-            <h3>que producto necesita: ${queproductoNecesita}</h3>
-            <h3>comentarios: ${comentarios}</h3>
-            <h3>comentarios: ${comentarios}</h3>
-            
-    `;
-    linea.innerHTML = content;
-    return linea;
-}
-
-// 6.recorrer todo el arbol de Dom y llevarnos al elemento data-tabla
-
-const tabla = document.querySelector("[data-tabla]")
-
-
-
-
-// abrir http (metodo, url)
 // CRUD -- METODOS HTTP
 //CREATE - POST
 //READ - GET
@@ -38,13 +10,7 @@ const listaClientes = () =>  fetch("http://localhost:3000/cliente").then((respue
 
 
 
-listaClientes().then((data) =>{
-    //7. se recibe los perfiles de cada uno de los clientes en j.son
-    data.forEach( perfil => {
-        const nuevaLinea = crearNuevaLinea(perfil.nombre, perfil.correoElectronico, perfil.edad, perfil.productodeInteres, perfil.recomendacion, perfil.mediodeComunicacion, perfil.queproductoNecesita, perfil.comentarios)
-        tabla.appendChild(nuevaLinea)
-    });
-}).catch((error) => alert("Ocurrio un error"))
+
     
 
 
---
+++ service/controllers/client-controller.js
@@ -0,0 +1,31 @@
+//funcion crear nueva linea donde se incluye los datos al html
+const crearNuevaLinea = (nombre, correoElectronico, edad, productodeInteres, recomendacion, mediodeComunicacion, queproductoNecesita, comentarios) =>{
+    const linea = document.createElement("div");
+    const content = `
+            <h3>NumeroCliente</h3>
+            <h3>Nombre:  ${nombre}</h3>
+            <h3>correo electronico:  ${correoElectronico}</h3>
+            <h3>edad: ${edad}</h3>
+            <h3>Producto Interes: ${productodeInteres}</h3>
+            <h3>recomendaciones: ${recomendacion}</h3>
+            <h3>medio de comunicacion: ${mediodeComunicacion}</h3>
+            <h3>que producto necesita: ${queproductoNecesita}</h3>
+            <h3>comentarios: ${comentarios}</h3>
+            <a href="eliminar">Eliminar</a>
+            <a href="editar">Editar</a>            
+            <h3>------------------------------------------------</h3>
+    `;
+    linea.innerHTML = content;
+    return linea;
+}
+
+//recorrer todo el arbol de Dom y llevarnos al elemento data-tabla
+const tabla = document.querySelector("[data-tabla]")
+
+//se recibe los perfiles de cada uno de los clientes en j.son
+listaClientes().then((data) =>{
+    data.forEach( perfil => {
+        const nuevaLinea = crearNuevaLinea(perfil.nombre, perfil.correoElectronico, perfil.edad, perfil.productodeInteres, perfil.recomendacion, perfil.mediodeComunicacion, perfil.queproductoNecesita, perfil.comentarios)
+        tabla.appendChild(nuevaLinea)
+    });
+}).catch((error) => alert("Ocurrio un error"))
